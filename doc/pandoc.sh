207ea73b6d3592870c4fc34af49f7af79766366d
M.P. Barzine <barzine@gmail.com>
2015-07-23 01:11:32 +0100

    pandoc update

--- doc/pandoc.sh
+++ doc/pandoc.sh
@@ -7,11 +7,11 @@
 #pandoc *.md -o ../pdf/practical.pdf --toc 
 ##          --variable title:"RNA-seq data analysis practical - Cancer Genomics Workshop" 
 ##          --variable date:"2015/07/23"
-##          --variable author:"Mitra P. Barzine and Nuno A. Fonseca"
+##          --variable author:"Mitra P. Barzine and Nuno A. Fonseca, Claudia Calabrese and Fatemeh Ghavidel"
 ##          --variable links-as-notes 
 ##          --variable linkcolor:black 
 ##          --variable urlcolor:black 
 ##          --variable geometry:margin=3cm
 
-pandoc *.md --latex-engine=xelatex -o ../pdf/practical.pdf --toc --variable title:"RNA-seq data analysis practical - Cancer Genomics Workshop" --variable date:"2015/07/23" --variable author:"Mitra P. Barzine and Nuno A. Fonseca" --variable links-as-notes --variable linkcolor:black --variable urlcolor:black --variable geometry:margin=3cm
+pandoc *.md --latex-engine=xelatex -o ../pdf/practical.pdf --toc --variable title:"RNA-seq data analysis practical - Cancer Genomics Workshop" --variable date:"2015/07/23" --variable author:"Mitra P. Barzine, Nuno A. Fonseca, Claudia Calabrese and Fatemeh Ghavidel" --variable links-as-notes --variable linkcolor:black --variable urlcolor:black --variable geometry:margin=3cm
 
